3809cfc508a9ab0e4b382023e8e92411af940235
aryan-a-2311 <aagar125@asu.edu>
2022-10-15 20:10:14 -0700

    Added the Login Functionality by reading buyer and seller txt

--- ptbs/Login.java
+++ ptbs/Login.java
@@ -1,4 +1,71 @@
 package ptbs;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Login {
+    Login() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--------------------------------LOGIN GUI--------------------------------");
+        System.out.println("Are you a:\n0.Buyer \nor a \n1.Seller?\n\n Enter 0 or 1: ");
+        int b_s = sc.nextInt();
+        if(b_s == 0 || b_s == 1)
+        {
+            System.out.println("Enter Username: ");
+            String uName = sc.next();
+            System.out.println("Enter Password: ");
+            String pWord = sc.next();
+            if(validate(uName, pWord, b_s)){
+                System.out.println("Logged In Successfully");
+            }
+            else{
+                System.out.println("Logged Failed");
+            }
+        }
+        else
+        {
+            System.out.println("Wrong Input");
+            System.exit(-1);
+        }
+    }
+
+    public boolean validate(String userName, String passWord, int buy_sell) throws Exception {
+        HashMap<String, String> loginMap= new HashMap<>();
+        File file = null;
+        if(buy_sell == 0)
+        {
+            file = new File("ptbs/BuyerInfo.txt");
+        }
+        else if (buy_sell == 1)
+        {
+            file = new File("ptbs/SellerInfo.txt");
+        }
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String tempStr;
+
+        while ((tempStr = reader.readLine()) != null) {
+            String tempLoginArray[];
+            tempLoginArray = tempStr.split(":");
+            loginMap.put(tempLoginArray[0], tempLoginArray[1]);
+        }
+
+        if(loginMap.containsKey(userName))
+        {
+            if(loginMap.get(userName).equals(passWord))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
---
+++ ptbs/Main.java
@@ -0,0 +1,10 @@
+package ptbs;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Login();
+    }
+}
